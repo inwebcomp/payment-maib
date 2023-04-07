2ac4b5e038184dc718c5864da253b743334bacdd
Escral <inwebcomp@gmail.com>
2023-04-07 23:55:52 +0300

    Payment drivers

--- src/App/MaibDriver.php
+++ src/App/MaibDriver.php
@@ -2,47 +2,154 @@
 
 namespace InWeb\Payment\Maib;
 
-use InWeb\Payment\Contracts\Payable;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use InWeb\Payment\Contracts\Driver\Revertable;
+use InWeb\Payment\Contracts\Driver\DayShouldBeClosed;
 use InWeb\Payment\Drivers\Driver;
 use InWeb\Payment\Models\Payment;
+use JetBrains\PhpStorm\NoReturn;
+use Maib\MaibApi\MaibClient;
 
-class MaibDriver extends Driver
+class MaibDriver extends Driver implements Revertable, DayShouldBeClosed
 {
-    // @todo Move to config
-    const CLIENT_HANDLER = 'https://maib.ecommerce.md:21443/ecomm/ClientHandler';
+    public mixed $config = [
+        'merchant_handler' => '',
+        'client_handler'   => '',
+        'certificate'      => [
+            'cert_file' => '',
+            'key_file'  => '',
+            'password'  => '',
+        ],
+        'language'         => 'en',
+        'method'           => 'sms',
+    ];
 
-    public function createPayment(Payment $payment, $successPath, $cancelPath, $buttonInfo = null)
+    protected MaibClient $client;
+
+    #[NoReturn] public function __construct()
+    {
+        $this->config = array_merge($this->config, config('services.payment-maib'));
+
+        $options = [
+            'base_uri' => $this->config['merchant_handler'],
+            'debug'    => false,
+            'verify'   => true,
+            'cert'     => [$this->config['certificate']['cert_file'], $this->config['certificate']['password']],
+            'ssl_key'  => $this->config['certificate']['key_file'],
+            'config'   => [
+                'curl' => [
+                    CURLOPT_SSL_VERIFYHOST => 2,
+                    CURLOPT_SSL_VERIFYPEER => true,
+                ]
+            ]
+        ];
+
+        $log = \Log::getLogger();
+        $stack = HandlerStack::create();
+        $stack->push(Middleware::log($log, new MessageFormatter(MessageFormatter::DEBUG)));
+
+        $options['handler'] = $stack;
+
+        $guzzleClient = new Client($options);
+
+        $this->client = new MaibClient($guzzleClient);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createPayment(Payment $payment, $successPath, $cancelPath, $buttonInfo = null): Payment
     {
         $transactionId = optional($payment->detail)['transaction_id'];
 
         if ($transactionId) {
-            return;
+            return Payment::where('transaction_id', $transactionId)->firstOrFail();
         }
 
         $transactionId = $this->registerTransaction($payment);
 
+        if (!$transactionId) {
+            throw new \Exception('Transaction registration failed');
+        }
+
         $payment->setDetail([
             'transaction_id' => $transactionId,
-            'gateway_url' => self::CLIENT_HANDLER . '?trans_id=' . urlencode($transactionId),
+            'gateway_url'    => $this->config['client_handler'] . '?trans_id=' . urlencode($transactionId),
         ]);
 
         $payment->save();
+
+        return $payment;
     }
 
-    public function getPaymentInfo(Payment $payment)
+    private function getTransactionInfo(Payment $payment): array
     {
-        // @todo Get maib transaction info
+        return $this->client->getTransactionResult($payment->detail['transaction_id'], '127.0.0.1');
+    }
 
-        return [
-            'result' => 'ok',
-            'card_number' => '5***********2372',
-        ];
+    public function isSuccessfulPayment(Payment $payment): bool
+    {
+        $transaction = $this->getTransactionInfo($payment);
+
+        return $transaction['status'] === 'OK';
     }
 
     private function registerTransaction(Payment $payment): string
     {
+        $transactionId = null;
+
+        if ($this->config['method'] === 'sms') {
+            $transactionId = $this->registerSmsTransaction($payment);
+        }
+
         // @todo Register maib transaction
 
-        return 'rEsfhyIk8s9ypxkcS9fj/3C8FqA=';
+        return $transactionId;
+    }
+
+    private function prepareTransactionInfo(Payment $payment): array
+    {
+        // The currency of the transaction - is the 3 digits code of currency from ISO 4217
+        $currency = 498; // MDL
+        // @todo Support multiple currencies in payments
+
+        return [
+            'amount'       => $payment->amount,
+            'currency'     => $currency,
+            'clientIpAddr' => '127.0.0.1', // The client IP address @todo
+            'description'  => $payment->payable->getPaymentDetail($payment->payer),
+            'lang'         => $this->config['language'] ?? 'en',
+            'redirect_url' => $this->config['client_handler'] . '?trans_id=',
+        ];
+    }
+
+    private function registerSmsTransaction(Payment $payment): string
+    {
+        $transactionInfo = $this->prepareTransactionInfo($payment);
+
+        $transaction = $this->client->registerSmsTransaction(
+            $transactionInfo['amount'],
+            $transactionInfo['currency'],
+            $transactionInfo['clientIpAddr'],
+            $transactionInfo['description'],
+            $transactionInfo['lang'],
+        );
+
+        return $transaction['TRANSACTION_ID'];
+    }
+
+    public function revertTransaction(Payment $payment, ?float $amount): bool
+    {
+        $response = $this->client->revertTransaction($payment->detail['transaction_id'], $amount ?? $payment->amount);
+
+        return $response['RESULT'] === 'OK';
+    }
+
+    public function closeDay(): void
+    {
+        $this->client->closeDay();
     }
 }
--- src/config/config.php
+++ src/config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+    'merchant_handler' => '',
+    'client_handler' => '',
 
+    'certificate' => [
+        'cert_file' => '',
+        'key_file' => '',
+        'password' => '',
+    ],
+
+    'language' => 'en',
+
+    'method' => 'sms',
 ];
